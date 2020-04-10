ec07866fc1db7fed56cf2cb64c0b72e5858cab37
Waine Junior <waine2000@gmail.com>
2020-04-10 15:53:25 -0300

    Updated post processing files
    Changed docstring format
    Changed number of zeros in files to read
    Added .exe to gitignore

--- src/Post Processing/dataSave.py
+++ src/Post Processing/dataSave.py
@@ -1,60 +1,74 @@
 from pyevtk.hl import gridToVTK
 from fileTreat import *
 
-'''
-    @brief Saves variables values to VTK format
-    @param macrsDict (dict()): dict with variable values and name as key 
-    @param filenameWrite (str): filename to write to (NO EXTENSION)
-    @param points (bool): if True, save as point centered data,
-                          if False, save as cell centered data
-    @param normVal (float): value to normalize distance. 
-                            If zero, the distance is normalized by NX
-'''
+
 def saveVTK3D(macrsDict, filenameWrite, points=True, normVal=0):
+    """ Saves variables values to VTK format
+
+    Parameters
+    ----------
+    macrsDict : dict()
+        Dict with variable values and name as key
+    filenameWrite : str
+        Filename to write to (NO EXTENSION)
+    points : bool, optional
+        If True, save as point centered data, if False,
+        save as cell centered data, by default True
+    normVal : int, optional
+        Value to normalize distance (if zero, the distance is
+        normalized by NX), by default 0
+    """
+
     info = getSimInfo()
-    
+
     if(normVal == 0):
         normVal = info['NX']
         if(points == True):
             normVal -= 1
 
-    dx, dy, dz = 1.0/normVal, 1.0/normVal, 1.0/normVal
+    dx, dy, dz = 1.0 / normVal, 1.0 / normVal, 1.0 / normVal
     if info['Prc'] == 'double':
         prc = 'float64'
     elif info['Prc'] == 'float':
         prc = 'float32'
-    
+
     if(points == False):
         # grid
-        x = np.arange(0, info['NX']/normVal+0.1*dx, dx, dtype=prc)
-        y = np.arange(0, info['NY']/normVal+0.1*dy, dy, dtype=prc)
-        z = np.arange(0, info['NZ']/normVal+0.1*dz, dz, dtype=prc)
-        gridToVTK(PATH+filenameWrite, x, y, z, cellData=macrsDict)
+        x = np.arange(0, info['NX'] / normVal + 0.1 * dx, dx, dtype=prc)
+        y = np.arange(0, info['NY'] / normVal + 0.1 * dy, dy, dtype=prc)
+        z = np.arange(0, info['NZ'] / normVal + 0.1 * dz, dz, dtype=prc)
+        gridToVTK(PATH + filenameWrite, x, y, z, cellData=macrsDict)
     else:
         # grid
-        x = np.arange(0, (info['NX']-1)/normVal+0.1*dx, dx, dtype=prc)
-        y = np.arange(0, (info['NY']-1)/normVal+0.1*dy, dy, dtype=prc)
-        z = np.arange(0, (info['NZ']-1)/normVal+0.1*dz, dz, dtype=prc)
-        gridToVTK(PATH+filenameWrite, x, y, z, pointData=macrsDict)
+        x = np.arange(0, (info['NX'] - 1) / normVal + 0.1 * dx, dx, dtype=prc)
+        y = np.arange(0, (info['NY'] - 1) / normVal + 0.1 * dy, dy, dtype=prc)
+        z = np.arange(0, (info['NZ'] - 1) / normVal + 0.1 * dz, dz, dtype=prc)
+        gridToVTK(PATH + filenameWrite, x, y, z, pointData=macrsDict)
 
 
-'''
-    @brief Saves macroscopics in a csv file
-    @param filenameWrite (str): filename to write to
-    @param macr (np.array()): array with macroscopics to save (1D or 2D)
-    @param normalizeDist (bool): normalize distance or not for 1D
-'''
 def saveMacrCsv(filenameWrite, macr, normalizeDist=False):
-    with open(PATH+filenameWrite, 'w') as f:
-        if(len(macr.shape) == 1): # 1D
+    """ Saves macroscopics in a csv file
+
+    Parameters
+    ----------
+    filenameWrite : str
+        Filename to write to
+    macr : np.array()
+        Array with macroscopics to save (1D or 2D)
+    normalizeDist : bool, optional
+        Normalize distance or not for 1D, by default False
+    """
+
+    with open(PATH + filenameWrite, 'w') as f:
+        if(len(macr.shape) == 1):  # 1D
             # csv is: position, value
             if(not normalizeDist):
                 array = [[i, macr[i]] for i in range(0, len(macr))]
                 np.savetxt(f, array, fmt=['%d', '%.6e'], delimiter=',')
             else:
-                array = [(i/len(macr), macr[i]) for i in range(0, len(macr))]
+                array = [(i / len(macr), macr[i]) for i in range(0, len(macr))]
                 np.savetxt(f, array, fmt=['%.6e', '%.6e'], delimiter=',')
-        elif(len(macr.shape) == 2): # 2D
+        elif(len(macr.shape) == 2):  # 2D
             np.savetxt(f, macr, delimiter=',')
         else:
             print("Input array for \"saveMacrCsv\" is not 2D or 1D")
--- src/Post Processing/fileTreat.py
+++ src/Post Processing/fileTreat.py
@@ -9,111 +9,144 @@ __macr_names__ = ['ux', 'uy', 'uz', 'rho']
 
 __info__ = dict()
 
-'''
-    @brief Get all macroscopics filenames from the folder
-    @param macrName (str): macroscopic name ('ux', 'uy', 'uz', 'rho')
-    @return list of macroscopics filenames
-'''
+
 def getFilenamesMacr(macrName):
-    listFiles = glob.glob(PATH+"*"+macrName+"*.bin")
-    listFiles.sort()
+    """ Get all macroscopics filenames from the folder
+
+    Parameters
+    ----------
+    macrName : str
+        Macroscopic name (__macr_name__)
+
+    Returns
+    -------
+    list()
+        List of macroscopics filenames
+    """
+
+    listFiles = sorted(glob.glob(PATH + "*" + macrName + "*.bin"))
     return listFiles
 
 
-'''
-    @brief Get all macroscopics steps from the folder
-    @return sorted list of steps values
-'''
 def getMacrSteps():
+    """ Get all macroscopics steps from the folder
+
+    Returns
+    -------
+    list()
+        Sorted list of steps values
+    """
+
     listFiles = getFilenamesMacr(__macr_names__[0])
     setMacrSteps = set()
     for i in listFiles:
-        macrStep = i.split(__macr_names__[0])[-1] # get everything after the macr name
-        macrStep = macrStep[:-4] # take off ".bin"
-        macrStep = int(macrStep) # convert to int
-        setMacrSteps.add(macrStep) # add macr step to set
-    listMacrSteps = list(setMacrSteps)
-    listMacrSteps.sort()
+        # get everything after the macr name
+        macrStep = i.split(__macr_names__[0])[-1]
+        macrStep = macrStep[:-4]  # take off ".bin"
+        macrStep = int(macrStep)  # convert to int
+        setMacrSteps.add(macrStep)  # add macr step to set
+    listMacrSteps = sorted(setMacrSteps)
     return listMacrSteps
 
 
-
-'''
-    @brief Get simulation info in dictionary format
-    @return dictionary with simulation info
-'''
 def getSimInfo():
+    """ Get simulation info in dictionary format
+
+    Returns
+    -------
+    dict()
+        dictionary with simulation info
+    """
+
     if len(__info__) == 0:
-        filename = glob.glob(PATH+"*info*.txt")[0]
+        filename = glob.glob(PATH + "*info*.txt")[0]
         with open(filename, "r") as f:
             lines = f.readlines()
             linesTrim = [l.strip() for l in lines]
-            
+
             try:
-                __info__['ID'] = [str(txt.split(" ")[-1]) for txt in linesTrim \
-                    if 'Simulation ID' in txt][0]
-            except:
+                __info__['ID'] = [str(txt.split(" ")[-1]) for txt in linesTrim
+                                  if 'Simulation ID' in txt][0]
+            except BaseException:
                 print("Not able to get ID from info file")
-            
+
             try:
-                __info__['Prc'] = [txt.split(" ")[-1] for txt in linesTrim if 'Precision' in txt][0]
-            except:
+                __info__['Prc'] = [
+                    txt.split(" ")[-1] for txt in linesTrim if 'Precision' in txt][0]
+            except BaseException:
                 print("Not able to get Precision from info file")
-            
+
             try:
-                __info__['NX'] = [int(txt.split(" ")[-1]) for txt in linesTrim if 'NX' in txt][0]
-            except:
+                __info__['NX'] = [int(txt.split(" ")[-1])
+                                  for txt in linesTrim if 'NX' in txt][0]
+            except BaseException:
                 print("Not able to get NX from info file")
-            
+
             try:
-                __info__['NY'] = [int(txt.split(" ")[-1]) for txt in linesTrim if 'NY' in txt][0]
-            except:
+                __info__['NY'] = [int(txt.split(" ")[-1])
+                                  for txt in linesTrim if 'NY' in txt][0]
+            except BaseException:
                 print("Not able to get NY from info file")
-            
+
             try:
-                __info__['NZ'] = [int(txt.split(" ")[-1]) for txt in linesTrim if 'NZ' in txt][0]
-            except:
+                __info__['NZ'] = [int(txt.split(" ")[-1])
+                                  for txt in linesTrim if 'NZ' in txt][0]
+            except BaseException:
                 print("Not able to get NZ from info file")
-            
+
             try:
-                __info__['Tau'] = [float(txt.split(" ")[-1]) for txt in linesTrim if 'Tau' in txt][0]
-            except:
+                __info__['Tau'] = [float(txt.split(" ")[-1])
+                                   for txt in linesTrim if 'Tau' in txt][0]
+            except BaseException:
                 print("Not able to get Tau from info file")
-            
+
             try:
-                __info__['Umax'] = [float(txt.split(" ")[-1]) for txt in linesTrim if 'Umax' in txt][0]
-            except:
+                __info__['Umax'] = [float(txt.split(" ")[-1])
+                                    for txt in linesTrim if 'Umax' in txt][0]
+            except BaseException:
                 print("Not able to get Umax from info file")
-            
+
             try:
-                __info__['Nsteps'] = [int(txt.split(" ")[-1]) for txt in linesTrim if 'Nsteps' in txt][0]
-            except:
+                __info__['Nsteps'] = [int(txt.split(" ")[-1])
+                                      for txt in linesTrim if 'Nsteps' in txt][0]
+            except BaseException:
                 print("Not able to get Nsteps from info file")
-            
+
             try:
-                __info__['FX'] = [float(txt.split(" ")[-1]) for txt in linesTrim if 'FX' in txt][0]
-            except:
+                __info__['FX'] = [float(txt.split(" ")[-1])
+                                  for txt in linesTrim if 'FX' in txt][0]
+            except BaseException:
                 print("Not able to get FX from info file")
-            
+
             try:
-                __info__['FY'] = [float(txt.split(" ")[-1]) for txt in linesTrim if 'FY' in txt][0]
-            except:
+                __info__['FY'] = [float(txt.split(" ")[-1])
+                                  for txt in linesTrim if 'FY' in txt][0]
+            except BaseException:
                 print("Not able to get FY from info file")
-            
+
             try:
-                __info__['FZ'] = [float(txt.split(" ")[-1]) for txt in linesTrim if 'FZ' in txt][0]
-            except:
+                __info__['FZ'] = [float(txt.split(" ")[-1])
+                                  for txt in linesTrim if 'FZ' in txt][0]
+            except BaseException:
                 print("Not able to get FZ from info file")
 
     return __info__
 
 
-'''
-    @brief Read the binary file and returns its content as a 3D matrix
-    @param macrFilename (str): filename of the macroscopic
-    @return macroscopic array (np.array([x, y, z]))
-'''
 def readFileMacr3D(macrFilename):
+    """ Read the binary file and returns its content as a 3D matrix
+
+    Parameters
+    ----------
+    macrFilename : str
+        Filename of the macroscopic
+
+    Returns
+    -------
+    np.array([x, y, z])
+        Macroscopic array
+    """
+
     info = getSimInfo()
     if info['Prc'] == 'double':
         prc = 'd'
@@ -125,21 +158,35 @@ def readFileMacr3D(macrFilename):
         return np.swapaxes(vec3D, 0, 2)
 
 
-'''
-    @brief Get all macroscopics in the folder from the step specified
-    @param step: step of the macroscopics
-    @return Dictionary with list of macroscopics  (dict(np.array()))
-'''
 def getMacrsFromStep(step):
+    """ Get all macroscopics in the folder from the step specified
+
+    Parameters
+    ----------
+    step : int
+        Step of the macroscopics
+
+    Returns
+    -------
+    dict(np.array())
+        Dictionary with list of macroscopics
+    """
+
     macr = dict()
     listFilename = list()
+
     for macrName in __macr_names__:
         listFilename.append(getFilenamesMacr(macrName))
+
     # flatten list
-    listFilenameFlat = [j for i in range(0, len(listFilename)) \
-        for j in listFilename[i]]
-    # get all filenames of the step
-    listFilenameStep = [i for i in listFilenameFlat if (("%05d" % step)+".bin") in i]
+    listFilenameFlat = [j for i in range(0, len(listFilename))
+                        for j in listFilename[i]]
+
+    # get all filenames of the step.
+    # THE NUMBER OF ZEROS MUST BE EQUAL TO THE ONE IN "lbmReport.cpp"
+    listFilenameStep = [
+        i for i in listFilenameFlat if (
+            ("%06d" % step) + ".bin") in i]
     # if there is no macroscopic from that step
     if len(listFilenameStep) == 0:
         return None
@@ -151,12 +198,17 @@ def getMacrsFromStep(step):
 
     return macr
 
-'''
-    @brief Get array for all macroscopics in folder. For compatibility all 
-            macrs must have the same number of files and same steps for each
-    @return Dictionary with list of macroscopics  (dict(list(np.array())))
-'''
+
 def getAllMacrs():
+    """ Get array for all macroscopics in folder. For compatibility all
+        macrs must have the same number of files and same steps for each
+
+    Returns
+    -------
+    dict(list(np.array()))
+        Dictionary with list of macroscopics
+    """
+
     macr = dict()
 
     filenames = dict()
@@ -166,13 +218,12 @@ def getAllMacrs():
     for i in range(0, min(len(filenames[i]) for i in filenames)):
         # getting simulation step
         macrStep = filenames[__macr_names__[0]][i].split(__macr_names__[0])[-1]
-        macrStep = macrStep[:-4] # take off ".bin"
+        macrStep = macrStep[:-4]  # take off ".bin"
         macrStep = int(macrStep)
-        
+
         # save macroscopics from simulation step
         macr[macrStep] = dict()
         for macrName in filenames:
             macr[macrStep][macrName] = readFileMacr3D(filenames[macrName][i])
-    
-    return macr
 
+    return macr
