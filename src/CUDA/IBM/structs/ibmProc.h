1de618d61f2dea6a70979193767b412487bc5cd7
Waine Junior <waine2000@gmail.com>
2020-11-05 16:51:44 -0300

    Updated IBM Proc

--- src/CUDA/IBM/structs/ibmProc.h
+++ src/CUDA/IBM/structs/ibmProc.h
@@ -1,49 +1,45 @@
 #ifndef __IBM_PROC_H
 #define __IBM_PROC_H
 
-#include "../globalFunctions.h"
+#include "../../globalFunctions.h"
 #include "../../structs/macroscopics.h"
 
 typedef struct ibmProc{
-    Macroscopics* macrCurr; // current macroscopics values
-    int* step;              // pointer to step value 
+    Macroscopics* macrCurr;  // current macroscopics values
+    ParticleCenter* pCenter; // current particle centers values
+    int* step;               // pointer to step value 
 
     // Treated values below
-    dfloat residual;
-    dfloat avgRho;
-    dfloat avgUzPlanXZ[NY]; // average Uz velocity in all XZ plans
 
     /* Constructor */
     __host__
-    macrProc()
+    ibmProc()
     {
         macrCurr = nullptr;
+        pCenter = nullptr;
         step = nullptr;
     }
 
     /* Destructor */
     __host__
-    ~macrProc()
+    ~ibmProc()
     {
         macrCurr = nullptr;
-        macrOld = nullptr;
+        pCenter = nullptr;
         step = nullptr;
-        residual = 0;
-        avgRho = 0;
     }
 
     /* Allocate necessary variables, if required dynamic allocation */
     __host__
-    void allocateMacrProc()
+    void allocateIBMProc()
     {
     }
 
     /* Free allocated variables, if required dynamic allocation */
     __host__
-    void freeMacrProc()
+    void freeIBMProc()
     {
     }
-
 }IBMProc;
 
 #endif // !__IBM_PROC_H
