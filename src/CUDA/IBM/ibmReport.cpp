b0a1838d07a51448751e36b6263fa3dd0f410dad
MarcoAurelioFerrari <marcoferrari@alunos.utfpr.edu.br>
2024-11-21 10:39:17 -0300

    removed S from node save

--- src/CUDA/IBM/ibmReport.cpp
+++ src/CUDA/IBM/ibmReport.cpp
@@ -74,7 +74,7 @@ void saveParticlesInfo(ParticlesSoA particles, unsigned int step, bool saveNodes
     outFilePCenter << strColumnNames << strValuesParticles.str();
 
     if(saveNodes){
-        strColumnNames = "particle_index" + sep + "pos_x" + sep + "pos_y" + sep + "pos_z" + sep + "S\n";
+        strColumnNames = "particle_index" + sep + "pos_x" + sep + "pos_y" + sep + "pos_z\n";
 
         std::ostringstream strValuesMesh("");
         strValuesMesh << std::scientific;
@@ -86,8 +86,7 @@ void saveParticlesInfo(ParticlesSoA particles, unsigned int step, bool saveNodes
             for(int i = 0; i < pnSoA.numNodes; i++){
                 dfloat3 pos = pnSoA.pos.getValuesFromIdx(i);
                 strValuesMesh << pnSoA.particleCenterIdx[i] << sep;
-                strValuesMesh << getStrDfloat3(pos, sep) << sep;
-                strValuesMesh << pnSoA.S[i] << "\n";
+                strValuesMesh << getStrDfloat3(pos, sep) << "\n";
             }
         }
 
